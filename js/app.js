5290dd66b79bb93c8e5ebc782b8e7dfe10a2a0fd
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-11-04 20:08:56 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 console.log('hello world! 🛬');
 
+const openBtn = document.querySelector('#btn__open');
+const form = document.querySelector('.fact-form');
 
+openBtn.addEventListener('click', () => {
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+  } else {
+    form.classList.add('hidden');
+  }
+});
