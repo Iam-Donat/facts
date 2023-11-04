8385f45fb77113a9237fe93515844632c90187de
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-11-04 20:11:06 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ const form = document.querySelector('.fact-form');
 openBtn.addEventListener('click', () => {
   if (form.classList.contains('hidden')) {
     form.classList.remove('hidden');
+    openBtn.textContent = 'Close';
   } else {
     form.classList.add('hidden');
+    openBtn.textContent = 'Share a Fact';
   }
 });
