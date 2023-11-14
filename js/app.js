089feb08812ebb009b3c2c3712b4b8220135a128
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-11-14 22:16:10 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
+
+// Selecting DOM elements
 const openBtn = document.querySelector('#btn__open');
 const form = document.querySelector('.fact-form');
 const factsList = document.querySelector('.facts-list');
 
+// Create DOM Element: Render facts in the list
 factsList.innerHTML = '';
 
+// Toggle form visibility
 openBtn.addEventListener('click', () => {
   if (form.classList.contains('hidden')) {
     form.classList.remove('hidden');
