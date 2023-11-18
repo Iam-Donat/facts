f1744127a5066dff44d0e726cfc735cdd3e0c987
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-11-18 11:30:07 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -32,6 +32,17 @@ const initialFacts = [
   }
 ];
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 // Selecting DOM elements
 const openBtn = document.querySelector('#btn__open');
 const form = document.querySelector('.fact-form');
@@ -72,7 +83,8 @@ function createFactsList(dataArray) {
     </p>
     <span
       class="tag"
-      style="background-color: #eab308">${fact.category}
+      style="background-color: ${CATEGORIES.find(cat => cat.name === fact.category).color
+      }">${fact.category}
     </span>
   </li>`
   );
