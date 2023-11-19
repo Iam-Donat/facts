20e8bd7874c281413c5d2b482cb4968fb154acbe
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-11-19 17:47:29 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <h1>Hello World! 👋</h1>
+  return (
+    // < !----------------header start---------------->
+    <header className="header">
+      <div className="logo">
+        <img
+          src="/logo.png"
+          alt="logo"
+        />
+        <h1>Today's Facts 📌</h1>
+      </div>
+
+      <button id="btn__open" className="btn btn-large">Share a fact</button>
+    </header>
+    // <!----------------header end---------------->
+  );
 }
 
 export default App;
